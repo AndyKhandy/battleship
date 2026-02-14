a8571385e17b4627c2c6657cfc0b2973737a98dd
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2026-02-14 12:44:53 -0600

    Styled and created dialog message for the winner

--- src/modules/DOM/displayWinner.js
+++ src/modules/DOM/displayWinner.js
@@ -1,13 +1,33 @@
-export function displayWinner(winnerName, winnerGridsHits, loserGridsHits)
+const winnerDialog = document.querySelector("#game-winner");
+const winnerMessage = document.querySelector("#game-winner h1");
+const playerHitDisplay = document.querySelector("#player-hits");
+const computerHitDisplay = document.querySelector("#computer-hits");
+
+
+export function displayWinner(winnerName, winnerGridsHits, loserGridsHits) {
+  winnerDialog.showModal();
+  if(winnerName == "fire-nation")
+  {
+    fireNationWin();
+  }
+  else{
+    waterTribeWin();
+  }
+  playerHitDisplay.textContent = "Player hits connected: " + loserGridsHits;
+  computerHitDisplay.textContent =
+    "Computer hits connected: " + winnerGridsHits;
+}
+
+function fireNationWin()
 {
-    const winnerDialog = document.querySelector("#game-winner");
-    const winnerMessage = document.querySelector("#game-winner p");
-    const playerHitDisplay = document.querySelector("#player-hits");
-    const computerHitDisplay = document.querySelector("#computer-hits");
+    winnerMessage.textContent = "DEFEAT!";
+    winnerMessage.classList.add("lost");
+    winnerDialog.classList.add("fire");
+}
 
+function waterTribeWin()
+{
+    winnerMessage.textContent = "VICTORY!";
+    winnerMessage.classList.add("won");
 
-    winnerDialog.showModal();
-    winnerMessage.textContent = winnerName + " won the game!";
-    playerHitDisplay.textContent = "Player hits connected: " + loserGridsHits;
-    computerHitDisplay.textContent = "Computer hits connected: " + winnerGridsHits;
 }
