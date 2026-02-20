7fbc17f1f5985ef0d5b57d125b19dc75b706cf56
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2026-02-19 22:41:19 -0600

    Feat: add help display to help user understanding how to play battleship + abilities

--- src/modules/DOM/ability.js
+++ src/modules/DOM/ability.js
@@ -3,3 +3,32 @@ export function changeAbilityBtnOpacity(abilityBtn,missedShots)
     let opacity = (70 + ((missedShots % 4)*10))/100;
     abilityBtn.style.opacity = opacity;
 }
+
+const fireDescriptBtn = document.querySelector("#fire-ability-more");
+const waterDescriptBtn = document.querySelector("#water-ability-more");
+const closeDialog = document.querySelector("#close-ability-info");
+const abilityDialog = document.querySelector("#ability-info");
+const helpBtn = document.querySelector("#help-button");
+
+helpBtn.addEventListener("click",()=>{
+    abilityDialog.showModal();
+})
+
+fireDescriptBtn.addEventListener("click",()=>{
+    toggleInformation("fire");
+});
+
+waterDescriptBtn.addEventListener("click",()=>{
+    toggleInformation("water");
+});
+
+function toggleInformation(abilityType)
+{
+    const description = document.querySelector(`#${abilityType}-description`);
+
+    description.classList.toggle("open");
+}
+
+closeDialog.addEventListener("click",()=>{
+    abilityDialog.close();
+})
